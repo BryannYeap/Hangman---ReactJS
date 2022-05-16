c2ebde2c6a9ff0f79b2b69b65eef56832387beac
BryannYeap <e0543723@u.nus.edu>
2022-05-17 02:11:12 +0800

    Add Word component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import Header from "./components/Header";
 import Drawing from "./components/Drawing";
+import Word from "./components/Word";
 import Keyboard from "./components/Keyboard";
 import "./App.css";
+
+const themes = ["Fruit", "Country", "School"];
+const words = {
+  Fruit: [
+    "APPLE",
+    "BANANA",
+    "CHERRY",
+    "LEMON",
+    "LIME",
+    "ORANGE",
+    "PEACH",
+    "RASPBERRY",
+    "STRAWBERRY",
+    "WATERMELON",
+  ],
+  Country: [
+    "AUSTRALIA",
+    "FINLAND",
+    "GERMANY",
+    "JAMAICA",
+    "MADAGASCAR",
+    "PAKISTAN",
+    "SINGAPORE",
+    "SOUTH AFRICA",
+    "SWITZERLAND",
+    "ZIMBABWE",
+  ],
+  School: [
+    "ASSEMBLY",
+    "CAFETERIA",
+    "DETENTION",
+    "DISCIPLINE MASTER",
+    "HOMEWORK",
+    "PHYSICAL EDUCATION",
+    "STATIONERY",
+    "UNIFORM",
+    "VALEDICTORIAN",
+  ],
+};
+
+let selectedTheme = themes[Math.floor(Math.random() * themes.length)];
+let setOfWords = words[selectedTheme];
+let selectedWord = setOfWords[Math.floor(Math.random() * setOfWords.length)];
 
 function App() {
   return (
     <>
       <Header />
       <Drawing />
+      <Word theme={selectedTheme} word={selectedWord} />
       <Keyboard />
     </>
   );
