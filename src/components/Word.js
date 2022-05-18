029c51739894a6763bd9cc60fc6dc57a59f3c653
BryannYeap <e0543723@u.nus.edu>
2022-05-19 00:07:34 +0800

    Refactor keyboard and add guessed keys

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-const Word = ({ theme, word }) => {
+const Word = ({ theme, word, correctLettersGuessed }) => {
   return (
     <>
       <div className="theme">Theme: {theme}</div>
-      <div className="word">{word}</div>
+      <div className="word">
+        {word.split("").map((letter, index) => {
+          return (
+            <span key={index}>
+              {letter == " " ? (
+                <>&nbsp;&nbsp;&nbsp;</>
+              ) : correctLettersGuessed.includes(letter) ? (
+                letter
+              ) : (
+                " _ "
+              )}
+            </span>
+          );
+        })}
+      </div>
     </>
   );
 };
