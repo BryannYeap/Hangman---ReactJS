2e8a682e208ccba03c7d371bbc1a45492f3e479a
BryannYeap <e0543723@u.nus.edu>
2022-05-19 01:15:01 +0800

    Add guessing logic

--- src/App.js
+++ src/App.js
@@ -50,21 +50,29 @@ let selectedWord = setOfWords[Math.floor(Math.random() * setOfWords.length)];
 
 function App() {
   const [playable, setPlayable] = useState(true);
-  const [correctLettersGuessed, setCorrectLettersGuessed] = useState([]);
-  const [wrongLettersGuessed, setWrongLettersGuessed] = useState([]);
+  const [lettersGuessed, setLettersGuessed] = useState([]);
+  const [numberOfWrongGuesses, setNumberOfWrongGuesses] = useState(0);
+
+  const guessKey = (key) => {
+    if (!lettersGuessed.includes(key)) {
+      setLettersGuessed([...lettersGuessed, key]);
+    }
+
+    if (!selectedWord.includes(key) && !lettersGuessed.includes(key)) {
+      setNumberOfWrongGuesses(numberOfWrongGuesses + 1);
+    }
+  };
 
   return (
     <>
       <Header />
-      <Drawing />
+      <Drawing numberOfWrongGuesses={numberOfWrongGuesses} />
       <Word
         theme={selectedTheme}
         word={selectedWord}
-        correctLettersGuessed={correctLettersGuessed}
+        lettersGuessed={lettersGuessed}
       />
-      <Keyboard
-        lettersGuessed={[...correctLettersGuessed, ...wrongLettersGuessed]}
-      />
+      <Keyboard onKeyAction={guessKey} lettersGuessed={lettersGuessed} />
     </>
   );
 }
