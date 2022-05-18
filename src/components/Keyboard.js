e98e8dbe41dba5c2f4a22b1970f382cf4c1db793
BryannYeap <e0543723@u.nus.edu>
2022-05-18 22:25:37 +0800

    Add event listeners for onclick

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Key from "./Key";
 
 const Keyboard = () => {
-  const line1Keys = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
-  const line2Keys = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
-  const line3Keys = ["Z", "X", "C", "V", "B", "N", "M"];
+  const handleClick = (key) => {};
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      console.log(e.key);
+    });
+  }, []);
+
+  const keyboardRows = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["Z", "X", "C", "V", "B", "N", "M"],
+  ];
+
+  const line1Keys = keyboardRows[0];
+  const line2Keys = keyboardRows[1];
+  const line3Keys = keyboardRows[2];
+  const allKeys = keyboardRows.flat();
 
   return (
     <div className="keyboard">
       <div className="line1">
         {line1Keys.map((key) => {
-          return <Key keyVal={key} />;
+          return (
+            <Key onClick={() => handleClick(key)} keyVal={key} key={key} />
+          );
         })}
       </div>
       <div className="line2">
         {line2Keys.map((key) => {
-          return <Key keyVal={key} />;
+          return (
+            <Key onClick={() => handleClick(key)} keyVal={key} key={key} />
+          );
         })}
       </div>
       <div className="line3">
         {line3Keys.map((key) => {
-          return <Key keyVal={key} />;
+          return (
+            <Key onClick={() => handleClick(key)} keyVal={key} key={key} />
+          );
         })}
       </div>
     </div>
